f03e5077eaa4a6d91e2727d88ce77352f4dbda7b
Torsten Edeler <torsten.edeler@haw-hamburg.de>
2017-08-17 19:49:13 +0200

    Added TAG to prevent output from being cleared

--- nbgrader/preprocessors/clearoutput.py
+++ nbgrader/preprocessors/clearoutput.py
@@ -1,5 +1,15 @@
 from nbconvert.preprocessors import ClearOutputPreprocessor
+from traitlets import Bool, Unicode
 from . import NbGraderPreprocessor
 
 class ClearOutput(NbGraderPreprocessor, ClearOutputPreprocessor):
-    pass
+    clear_deaktivate_id = Unicode(
+        "DO NOT CLEAR OUTPUT",
+        help="if ### <clear_deaktivate_id> is the first entry in a cell the output is not cleared"
+    ).tag(config=True)
+
+    def preprocess_cell(self, cell, resources, cell_index):
+        if cell.source.startswith('### ' + self.clear_deaktivate_id):
+            return cell, resources
+        else:
+            return super(ClearOutput, self).preprocess_cell(cell, resources, cell_index)
